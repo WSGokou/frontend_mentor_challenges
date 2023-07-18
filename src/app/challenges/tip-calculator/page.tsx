aaa7c76ce6bc515f171ca17e674fd460884a3b9a
William Okou <gokou097@gmail.com>
2023-07-18 12:38:17 +0100

    Big screen styles and reset button disabled styles

--- src/app/challenges/tip-calculator/page.tsx
+++ src/app/challenges/tip-calculator/page.tsx
@@ -17,7 +17,7 @@ const totalTextStyles = {
   text: 'flex flex-col',
   header: 'text-[#f4fafa] text-base',
   para: 'text-sm',
-  amount: 'text-[#26c0ab] text-3xl',
+  amount: 'text-[#26c0ab] text-3xl lg:text-4xl',
 };
 // Total container 	#00494d
 // Tip/peoplenumber #5e7a7d
@@ -30,6 +30,7 @@ const TipCalculatorPage = () => {
   const [bill, setBill] = useState<number>(0);
   const [numPeople, setNumPeople] = useState<number>(0);
   const [selectedTip, setSelectedTip] = useState<number>(0);
+  const [customTip, setCustomTip] = useState<number>(0);
   const tipPercentages = [
     {label: '5%', value: 0.05},
     {label: '10%', value: 0.1},
@@ -48,14 +49,17 @@ const TipCalculatorPage = () => {
     setBill(0);
     setNumPeople(0);
     setSelectedTip(0);
+    setCustomTip(0);
   };
+
+  const resetButtonDisabled = !numPeople && !bill && !customTip;
 
   return (
     <div
       className={`${spaceMono.className} bg-[#c5e4e7] h-full w-screen flex flex-col items-center text-2xl text-[#5e7a7d]`}
     >
       {/* App Container */}
-      <div className="pt-12 flex flex-col items-center gap-10">
+      <div className="pt-12 lg:pt-40 flex flex-col items-center gap-10 lg:gap-[88px]">
         {/* Logo container */}
         <div>
           <Image
@@ -64,102 +68,107 @@ const TipCalculatorPage = () => {
           />
         </div>
         {/* Calculator container */}
-        <div className="bg-white grid grid-cols-1 rounded-t-2xl px-6 pt-10 pb-8 w-[375px]">
-          {/* Bill input */}
-          <div className="flex flex-col gap-3 mb-10">
-            <label
-              htmlFor="bill"
-              className="text-sm"
-            >
-              Bill
-            </label>
-            <div className="h-12 flex flex-row relative items-center">
-              <Image
-                src={dollarIcon}
-                alt=""
-                className="absolute left-4"
-              />
-              <input
-                type="number"
-                name="bill"
-                id="bill-input"
-                placeholder="0"
-                value={bill > 0 ? bill : ''}
-                className={`${hideInputArrows} text-right text-[#00494d] h-full bg-[#f4fafa] rounded-md w-full pl-9 pr-5`}
-                onChange={(e) => {
-                  setBill(Number(e.target.value));
-                }}
-              />
+        <div className="bg-white grid grid-cols-1 lg:grid-cols-2 lg:grid-flow-col lg:gap-12 rounded-t-2xl lg:rounded-2xl px-6 pt-10 pb-8 lg:p-8 w-[375px] lg:w-[920px] lg:h-[482px] lg:shadow-2xl">
+          {/* Choices container */}
+          <div className="w-full flex flex-col">
+            {/* Bill input */}
+            <div className="flex flex-col gap-3 mb-10">
+              <label
+                htmlFor="bill"
+                className="text-sm"
+              >
+                Bill
+              </label>
+              <div className="h-12 flex flex-row relative items-center">
+                <Image
+                  src={dollarIcon}
+                  alt=""
+                  className="absolute left-4"
+                />
+                <input
+                  type="number"
+                  name="bill"
+                  id="bill-input"
+                  placeholder="0"
+                  value={bill > 0 ? bill : ''}
+                  className={`${hideInputArrows} text-right text-[#00494d] h-full bg-[#f4fafa] rounded-md w-full pl-9 pr-5`}
+                  onChange={(e) => {
+                    setBill(Number(e.target.value));
+                  }}
+                />
+              </div>
             </div>
-          </div>
-          {/* Tip percentage */}
-          <div className="mb-10 flex flex-col gap-5">
-            <h4 className="text-sm">Select Tip %</h4>
-            <div className="grid grid-cols-2 gap-4 text-[#f4fafa]">
-              {tipPercentages.map((tip, idx) => (
-                <div
-                  key={idx}
-                  className={`flex items-center justify-center text-center`}
-                >
-                  <input
-                    type="radio"
-                    id={tip.label}
-                    value={tip.value}
-                    name="tip-select"
-                    className={`peer appearance-none`}
-                    checked={selectedTip === tip.value}
-                    onChange={(e) => {
-                      setSelectedTip(Number(e.target.value));
-                    }}
-                  />
-                  <label
-                    htmlFor={tip.label}
-                    className={`w-full bg-[#00494d] hover:bg-[#26c0ab70] cursor-pointer hover:text-[#00494d] peer-checked:text-[#00494d] peer-checked:bg-[#26c0ab] py-2 rounded-md`}
+            {/* Tip percentage */}
+            <div className="mb-10 flex flex-col gap-5">
+              <h4 className="text-sm">Select Tip %</h4>
+              <div className="grid grid-cols-2 lg:grid-cols-3 font-light gap-4 text-[#f4fafa]">
+                {tipPercentages.map((tip, idx) => (
+                  <div
+                    key={idx}
+                    className={`flex items-center justify-center text-center`}
                   >
-                    {tip.label}
-                  </label>
-                </div>
-              ))}
-              <input
-                type="number"
-                placeholder="Custom"
-                onChange={(e) => {
-                  setSelectedTip(Number(e.target.value) / 100);
-                }}
-                className={`${hideInputArrows} bg-[#f4fafa] pl-5 pr-5 text-right text-[#00494d] rounded-sm placeholder:text-[#5e7a7d]`}
-              />
+                    <input
+                      type="radio"
+                      id={tip.label}
+                      value={tip.value}
+                      name="tip-select"
+                      className={`peer appearance-none`}
+                      checked={selectedTip === tip.value}
+                      onChange={(e) => {
+                        setSelectedTip(Number(e.target.value));
+                      }}
+                    />
+                    <label
+                      htmlFor={tip.label}
+                      className={`w-full bg-[#00494d] hover:bg-[#26c0ab70] cursor-pointer hover:text-[#00494d] peer-checked:text-[#00494d] peer-checked:bg-[#26c0ab] py-2 rounded-md`}
+                    >
+                      {tip.label}
+                    </label>
+                  </div>
+                ))}
+                <input
+                  type="number"
+                  placeholder="Custom"
+                  value={customTip > 0 ? customTip : ''}
+                  onChange={(e) => {
+                    setSelectedTip(Number(e.target.value) / 100);
+                    setCustomTip(Number(e.target.value));
+                  }}
+                  className={`${hideInputArrows} bg-[#f4fafa] px-5 lg:px-3 text-right text-[#00494d] rounded-sm placeholder:text-[#5e7a7d]`}
+                />
+              </div>
             </div>
-          </div>
-          {/* Number of people input */}
-          <div className="flex flex-col gap-3 mb-9">
-            <label
-              htmlFor="people"
-              className="text-sm"
-            >
-              Number of People
-            </label>
-            <div className="h-12 flex flex-row relative items-center">
-              <Image
-                src={personIcon}
-                alt=""
-                className="absolute left-4"
-              />
-              <input
-                type="number"
-                name="people"
-                placeholder="0"
-                value={numPeople > 0 ? numPeople : ''}
-                min={1}
-                className={`${hideInputArrows} text-right text-[#00494d] h-full bg-[#f4fafa] rounded-md w-full pl-9 pr-5`}
-                onChange={(e) => {
-                  setNumPeople(Number(e.target.value));
-                }}
-              />
+            {/* Number of people input */}
+            <div className="flex flex-col gap-3 mb-9 lg:mb-0">
+              <label
+                htmlFor="people"
+                className="text-sm"
+              >
+                Number of People
+              </label>
+              <div className="h-12 flex flex-row relative items-center">
+                <Image
+                  src={personIcon}
+                  alt=""
+                  className="absolute left-4"
+                />
+                <input
+                  type="number"
+                  name="people"
+                  placeholder="0"
+                  value={numPeople > 0 ? numPeople : ''}
+                  min={1}
+                  className={`${hideInputArrows} text-right text-[#00494d] h-full bg-[#f4fafa] rounded-md w-full pl-9 pr-5`}
+                  onChange={(e) => {
+                    setNumPeople(Number(e.target.value));
+                  }}
+                />
+              </div>
             </div>
           </div>
 
           {/* Calculations container */}
-          <div className="bg-[#00494d] p-6 pt-11 flex flex-col gap-8 rounded-2xl">
+          <div className="bg-[#00494d] w-full p-6 pt-11 lg:pt-16 lg:pb-10 lg:px-9 flex flex-col gap-8 rounded-2xl">
             {/* Tip Amount */}
             <div className={`${totalTextStyles.container}`}>
               {/* Text */}
@@ -184,10 +193,15 @@ const TipCalculatorPage = () => {
                 className={`${totalTextStyles.amount}`}
               >{`$${totalPerPerson}`}</div>
             </div>
-            <div className="bg-white w-full rounded-md">
+            <div
+              className={`bg-[#ffffff] lg:mt-auto w-full rounded-md ${
+                !resetButtonDisabled ? '' : 'opacity-25'
+              }`}
+            >
               <button
                 onClick={onReset}
-                className="bg-[#26c0ab] hover:bg-[#26c0ab70] text-[#00494d] text-xl rounded-md py-3 w-full"
+                className="bg-[#26c0ab] enabled:hover:bg-[#26c0ab70] text-[#00494d] text-xl rounded-md py-3 w-full"
+                disabled={resetButtonDisabled}
               >
                 RESET
               </button>
