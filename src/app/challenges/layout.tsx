c3c7d218d2c9e5f6d5861ac0ba0fa639131ed9cb
William Okou <gokou097@gmail.com>
2023-07-22 21:40:19 +0100

    challenges button now stays at bottom

--- src/app/challenges/layout.tsx
+++ src/app/challenges/layout.tsx
@@ -6,14 +6,16 @@ export default function ChallengesLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col h-screen min-h-fit">
+    <div className="flex flex-col h-screen min-h-fit relative">
       {children}
-      <Link
-        href={'/challenges'}
-        className="bg-blue-900 text-white rounded-md p-2 self-start m-1 absolute -bottom-20"
-      >
-        Challenges
-      </Link>
+      <div className="relative">
+        <Link
+          href={'/challenges'}
+          className="bg-blue-900 text-white rounded-md p-2 m-1 absolute -bottom-20"
+        >
+          Challenges
+        </Link>
+      </div>
     </div>
   );
 }
